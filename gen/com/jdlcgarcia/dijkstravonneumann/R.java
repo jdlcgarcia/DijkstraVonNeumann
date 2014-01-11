ff9c13bca2697136b1552a9d0cd511e9fbcd7a83
Juan de la Cruz García García <jcruzgar@gmail.com>
2014-01-11 03:49:52 +0100

    Terminado
    A falta de hacer un texto medianamente original.

--- gen/com/jdlcgarcia/dijkstravonneumann/R.java
+++ gen/com/jdlcgarcia/dijkstravonneumann/R.java
@@ -26,9 +26,11 @@ public final class R {
         public static final int vonneumann=0x7f020002;
     }
     public static final class id {
-        public static final int action_settings=0x7f080002;
-        public static final int imageView1=0x7f080001;
-        public static final int textView1=0x7f080000;
+        public static final int IVmainpic=0x7f080001;
+        public static final int SVdescription=0x7f080002;
+        public static final int TVtitle=0x7f080000;
+        public static final int action_settings=0x7f080004;
+        public static final int description=0x7f080003;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -37,9 +39,14 @@ public final class R {
         public static final int main=0x7f070000;
     }
     public static final class string {
+        public static final int CD_dijkstra=0x7f050006;
+        public static final int CD_vonneumann=0x7f050003;
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050002;
+        public static final int desc_dijkstra=0x7f050007;
+        public static final int desc_vonneumann=0x7f050004;
+        public static final int title_dijkstra=0x7f050005;
+        public static final int title_vonneumann=0x7f050002;
     }
     public static final class style {
         /** 
